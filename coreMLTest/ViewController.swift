5ab99196569479f4d7565dcc32690c906d425dc4
abdul diallo <rahimi.diallo96@gmail.com>
2020-02-12 09:39:07 -0500

    Initial Commit

---
+++ coreMLTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  coreMLTest
+//
+//  Created by Abdul Diallo on 2/12/20.
+//  Copyright © 2020 Abdul Diallo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
