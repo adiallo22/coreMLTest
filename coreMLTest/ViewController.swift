abc29f8a220b5922dc6e8cd0508777f0b0163db8
abdul diallo <rahimi.diallo96@gmail.com>
2020-02-17 09:38:09 -0500

    make request. handle the result.

--- coreMLTest/ViewController.swift
+++ coreMLTest/ViewController.swift
@@ -14,7 +14,7 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
     
     @IBOutlet weak var imageDisplay: UIImageView!
     
-    let picker = UIImagePickerController()
+    private let picker = UIImagePickerController()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -24,14 +24,47 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
     }
     
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        
         if let usrImg = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
             imageDisplay.image = usrImg
+            guard let mlImage = CIImage(image: usrImg) else {
+                fatalError("Conversion into CIImage failed.")
+            }
+            detect(image: mlImage)
         }
+        
         picker.dismiss(animated: true, completion: nil)
+        
+    }
+    
+    func detect(image: CIImage) {
+        
+        guard let model = try? VNCoreMLModel(for: Inceptionv3().model) else {
+            fatalError("Failed loading CoreML...")
+        }
+        
+        let request = VNCoreMLRequest(model: model) { (request, error) in
+            guard let results = request.results as? [VNClassificationObservation] else {
+                fatalError("Enable to process the request")
+            }
+            if let found = results.first {
+                self.navigationItem.title = found.identifier
+            }
+        }
+        
+        let handler = VNImageRequestHandler(ciImage: image)
+        do {
+            try handler.perform([request])
+        } catch {
+            print("Failed to handle the request - \(error)")
+        }
+        
     }
 
     @IBAction func camBtn(_ sender: UIBarButtonItem) {
+        
         present(picker, animated: false, completion: nil)
+        
     }
     
     
