bea80ad8540076b133e9b8e6332ac73be34e043f
abdul diallo <rahimi.diallo96@gmail.com>
2020-02-17 08:13:46 -0500

    imported the framework CoreML and Vision. Add 2 delegated (one for the UIImagePicker, one for Navigation Cam button). Set delegate as self. Initialize a new object picker

--- coreMLTest/ViewController.swift
+++ coreMLTest/ViewController.swift
@@ -7,14 +7,34 @@
 //
 
 import UIKit
+import CoreML
+import Vision
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    @IBOutlet weak var imageDisplay: UIImageView!
+    
+    let picker = UIImagePickerController()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        picker.delegate = self
+        picker.sourceType = .photoLibrary
+        picker.allowsEditing = false
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        if let usrImg = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
+            imageDisplay.image = usrImg
+        }
+        picker.dismiss(animated: true, completion: nil)
     }
 
-
+    @IBAction func camBtn(_ sender: UIBarButtonItem) {
+        present(picker, animated: false, completion: nil)
+    }
+    
+    
+    
 }
 
